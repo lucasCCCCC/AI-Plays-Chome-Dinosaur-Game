118604ea01b9ec54bc4ee6c88f41bfd556df30ca
lucascdri <93845714+lucascdri@users.noreply.github.com>
2022-04-26 16:11:33 +0100

    Created the dinosaur class

---
+++ dinosaur.py
@@ -0,0 +1,52 @@
+import pygame
+
+DINOSAUR_RUN = [pygame.image.load("Game Assets/Dinosaur/DinosaurRun1.png"),
+                pygame.image.load("Game Assets/Dinosaur/DinosaurRun2.png")]
+
+DINOSAUR_JUMP = pygame.image.load("Game Assets/Dinosaur/DinosaurJump.png")
+
+
+class Dinosaur:
+    X = 70
+    Y = 360
+    speed_jump = 5
+
+    def __init__(self, img=DINOSAUR_RUN[0]):
+        self.img = img
+        self.imgStep = 0
+        self.JUMP = False
+        self.jump_speed = self.speed_jump
+        self.RUN = True
+        self.rect = pygame.Rect(self.X, self.Y, img.get_width(), img.get_height())
+
+    def updateDinosaur(self):
+        if self.JUMP:
+            self.jump()
+
+        if self.RUN:
+            self.run()
+
+        if self.imgStep >= 40:
+            self.imgStep = 0
+
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.img, (self.rect.x, self.rect.y))
+
+    def jump(self):
+        self.img = DINOSAUR_JUMP
+
+        if self.JUMP:
+            self.rect.y -= self.speed_jump * 2
+            self.speed_jump -= 0.5
+
+        if self.jump_speed <= -self.speed_jump:
+            self.JUMP = False
+            self.RUN = True
+            self.jump_speed = self.speed_jump
+
+    def run(self):
+        self.img = DINOSAUR_RUN[self.imgStep // 20]
+        self.rect.x = self.X
+        self.rect.y = self.Y
+        self.imgStep += 1
