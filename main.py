5ce049b981e06ff5283d870c875a623f52977e15
lucascdri <93845714+lucascdri@users.noreply.github.com>
2022-04-27 10:07:47 +0100

    Completed game logic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
+from random import randint
 from dinosaur import Dinosaur
+from cactus import SmallCactus, LargeCactus
 
 pygame.init()
 
@@ -8,19 +10,28 @@ SCREEN_HEIGHT = 600
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 FONT = pygame.font.SysFont("system", 40)
 
-
 FLOOR = pygame.image.load("Game Assets/Floor/Floor.png")
 
+LARGE_CACTI = [pygame.image.load("Game Assets/Cacti/LargeCacti21.png"),
+               pygame.image.load("Game Assets/Cacti/LargeCacti22.png"),
+               pygame.image.load("Game Assets/Cacti/LargeCacti23.png")]
+
+SMALL_CACTI = [pygame.image.load("Game Assets/Cacti/SmallCacti11.png"),
+               pygame.image.load("Game Assets/Cacti/SmallCacti12.png"),
+               pygame.image.load("Game Assets/Cacti/SmallCacti13.png")]
+
+
 def main():
+    global score, speed, xFloor, yFloor, dinosaurs, cacti
 
     clock = pygame.time.Clock()
     dinosaurs = [Dinosaur()]
 
-    global score, speed, xFloor, yFloor
     score = 0
     speed = 10
     xFloor = 0
     yFloor = 443
+    cacti = []
 
     def displayScore():
         global score, speed
@@ -40,6 +51,8 @@ def main():
             xFloor = 0
         xFloor -= speed
 
+    def removeDinosaur(d):
+        dinosaurs.pop(d)
 
     running = True
 
@@ -54,10 +67,29 @@ def main():
             dino.updateDinosaur()
             dino.draw(SCREEN)
 
+        if len(dinosaurs) == 0:
+            break
+
+        if len(cacti) == 0:
+            choice = randint(0, 1)
+
+            if choice == 0:
+                cacti.append(SmallCactus(SMALL_CACTI, randint(0, 2)))
+
+            if choice == 1:
+                cacti.append(LargeCactus(LARGE_CACTI, randint(0, 2)))
+
+        for cacti_ in cacti:
+            cacti_.draw(SCREEN)
+            cacti_.update(speed, cacti)
+
+            for x, dino in enumerate(dinosaurs):
+                if dino.rect.colliderect(cacti_.rect):
+                    removeDinosaur(x)
 
         keyPressed = pygame.key.get_pressed()
 
-        for x, dino in enumerate(dinosaurs):
+        for dino in dinosaurs:
             if keyPressed[pygame.K_UP] or keyPressed[pygame.K_SPACE]:
                 dino.JUMP = True
                 dino.RUN = False
@@ -69,5 +101,6 @@ def main():
 
     pygame.quit()
     quit()
+
 
 main()
