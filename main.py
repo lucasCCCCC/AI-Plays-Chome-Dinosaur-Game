0577f91ba7997f1c8b49cf1f5087cd9b7e4f3045
lucascdri <93845714+lucascdri@users.noreply.github.com>
2022-04-28 11:37:31 +0100

    Implemented game AI using the NEAT library

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import neat
 import pygame
+import os
+import math
 from random import randint
 from dinosaur import Dinosaur
 from cactus import SmallCactus, LargeCactus
@@ -9,6 +12,8 @@ SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 600
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 FONT = pygame.font.SysFont("system", 40)
+FONT2 = pygame.font.SysFont("system", 20)
+
 
 FLOOR = pygame.image.load("Game Assets/Floor/Floor.png")
 
@@ -21,17 +26,33 @@ SMALL_CACTI = [pygame.image.load("Game Assets/Cacti/SmallCacti11.png"),
                pygame.image.load("Game Assets/Cacti/SmallCacti13.png")]
 
 
-def main():
-    global score, speed, xFloor, yFloor, dinosaurs, cacti
+def eval_genomes(genomes, config):
+
+    global score, speed, xFloor, yFloor, dinosaurs, cacti, ge, nets
 
     clock = pygame.time.Clock()
-    dinosaurs = [Dinosaur()]
 
     score = 0
     speed = 10
     xFloor = 0
     yFloor = 443
     cacti = []
+
+    dinosaurs = []
+    ge = []
+    nets = []
+
+    for genome_id, genome in genomes:
+        genome.fitness = 0
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        nets.append(net)
+        dinosaurs.append(Dinosaur())
+        ge.append(genome)
+
+
+    def computeDistance(d, c):
+        return math.sqrt((d[0]-c[0])**2 + (d[1]-c[1])**2)
+
 
     def displayScore():
         global score, speed
@@ -42,6 +63,15 @@ def main():
         s = FONT.render(str(score), True, "white")
         SCREEN.blit(s, [750, 25])
 
+
+    def displayGeneration():
+        g = FONT2.render("Generation: " + str(population.generation + 1), True, "white")
+        p = FONT2.render("Population: " + str(len(dinosaurs)), True, "white")
+
+        SCREEN.blit(g, [25, 25])
+        SCREEN.blit(p, [25, 50])
+
+
     def animateGround():
         global xFloor, yFloor
         SCREEN.blit(FLOOR, [xFloor, yFloor])
@@ -51,8 +81,12 @@ def main():
             xFloor = 0
         xFloor -= speed
 
+
     def removeDinosaur(d):
         dinosaurs.pop(d)
+        nets.pop(d)
+        ge.pop(d)
+
 
     running = True
 
@@ -60,6 +94,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                pygame.quit()
+                quit()
 
         SCREEN.fill((39, 41, 43))
 
@@ -79,28 +115,52 @@ def main():
             if choice == 1:
                 cacti.append(LargeCactus(LARGE_CACTI, randint(0, 2)))
 
+
         for cacti_ in cacti:
             cacti_.draw(SCREEN)
             cacti_.update(speed, cacti)
 
             for x, dino in enumerate(dinosaurs):
                 if dino.rect.colliderect(cacti_.rect):
+                    ge[x].fitness -= 1
                     removeDinosaur(x)
 
-        keyPressed = pygame.key.get_pressed()
+        # keyPressed = pygame.key.get_pressed()
 
-        for dino in dinosaurs:
-            if keyPressed[pygame.K_UP] or keyPressed[pygame.K_SPACE]:
+        # for dino in dinosaurs:
+            # if keyPressed[pygame.K_UP] or keyPressed[pygame.K_SPACE]:
+                # dino.JUMP = True
+                # dino.RUN = False
+
+        for x, dino in enumerate(dinosaurs):
+            nnOutput = nets[x].activate((dino.rect.y, computeDistance((dino.rect.x, dino.rect.y),
+                                                                      cacti_.rect.midtop)))
+
+            if nnOutput[0] > 0.5 and dino.rect.y == dino.Y:
                 dino.JUMP = True
                 dino.RUN = False
 
+
         displayScore()
         animateGround()
+        displayGeneration()
+
         clock.tick(45)
         pygame.display.update()
 
-    pygame.quit()
-    quit()
+
+def run(config_file):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                                config_file)
+
+    global population
+
+    population = neat.Population(config)
+    population.run(eval_genomes, 50)
 
 
-main()
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
+    run(config_path)
